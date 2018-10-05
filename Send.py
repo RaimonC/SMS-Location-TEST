a9299e739f4142378f7f451d977f872b8046a6bf
Simranjeet Singh <38141273+simranjeet97@users.noreply.github.com>
2018-10-05 15:42:47 +0530

    Update Send.py

--- Send.py
+++ Send.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
-account_sid = 'ACce7ca982c8ee3a79f64bb9082ead98a0'
-auth_token = 'e6e1d7e1bb4cd13e91ee6bc4a7fd1ce8'
+account_sid = 'Get From Twilio Consol'
+auth_token = 'Get From Twilio Consol'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
